a943aa760adf2a9fa946d22a3d55fad02e071ed1
chaeyoung <devchae9@gmail.com>
2019-02-25 10:50:54 +0900

    Working ver.

--- Baekjoon_Complete/src/q15662_cogwheel.java
+++ Baekjoon_Complete/src/q15662_cogwheel.java
@@ -1,111 +1,110 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
+
 
 public class q15662_cogwheel {
 	static int[][] cog;
 	static boolean[] visit;
+	static Stack<listto> st = new Stack<listto>();
 	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	public static void main(String args[]) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		cog = new int[T+1][8];
 		visit = new boolean[T+1];
 		
 		for (int i = 1 ; i < T+1; i++) {
-			String a = sc.next();
+			
+			String a = br.readLine();
 			for (int j = 0 ; j < 8; j++) {
 				cog[i][j] = Integer.parseInt(a.substring(j, j+1));
 			}
 		}
 		
-		int K = sc.nextInt();
-		
+		int K = Integer.parseInt(br.readLine());
 		int n[] = new int[K];
 		int d[] = new int[K];
 
 		for (int i = 0 ; i < K; i++) {
-			n[i] = sc.nextInt();
-			d[i] = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n[i] = Integer.parseInt(st.nextToken());
+			d[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		printaa();
 		for (int i = 0 ; i < K; i++) {
 			visit[n[i]] = true;
+			st.add(new listto(n[i], d[i]));
 			check(d[i], n[i], T);
 			visit = new boolean[T+1];
-			printaa();
+			
+			while(!st.isEmpty()) {
+				listto l = st.pop();
+				rotate(l.dir, l.num);
+			}
 		}
+		
+		int ans = 0;
+		for (int i = 1; i <= T; i++ ) {
+			ans += cog[i][0];
+		}
+		System.out.println(ans);
 	}
 	
-	static void printaa() {
-		for (int i = 1 ; i < 5; i++) {
-			for (int j = 0 ; j < 8; j++) {
-				System.out.print(cog[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("_______________");
-	}
 	
 	static void check(int dir, int num, int max) {
 		if (num <0 || num > max) return;
 		
-		
 		if (num -1 > 0) {
 			if (cog[num][6] != cog[num-1][2] && visit[num-1] == false) {
-				System.out.format("checking [%d][%d] %d = [%d][%d] %d\n", num,6, cog[num][6], num-1, 2, cog[num-1][2]);
 				visit[num-1] = true;
+				st.push(new listto(num-1, -dir));
 				if (num -1 != 1) {
 					check(-dir, num-1, max);
-				} else if (num - 1 == 0) {
-					rotate(-dir, num-1);
 				}
-				rotate(dir, num);
-				return;
 			}
 		}
 		
-		if (num+1 < max+1) {
+		if (num+1 < max +1) {
 			if (cog[num][2] != cog[num+1][6] && visit[num +1] == false) {
-				System.out.format("checking [%d][%d] %d = [%d][%d] %d\n", num,2, cog[num][2], num+1, 6, cog[num+1][6]);
 				visit[num+1] = true;
+				st.push(new listto(num+1, -dir));
 				if (num+1 != max+1) {
 					check(-dir, num+1, max);
-				} else if (num+1 == max+1) {
-					rotate(-dir, num+1);
-				}
-				rotate(dir, num);
-				return;
+				} 
 			}
 		}
-		
-		
-	
 	}
 	
 	static void rotate( int dir, int num) {
 		switch(dir) {
-		case -1:{
-			int temp = cog[num][0];
-			for (int i = 0 ; i < 7; i++) {
-				cog[num][i] = cog[num][i+1];
+			case -1:{
+				int temp = cog[num][0];
+				for (int i = 0 ; i < 7; i++) {
+					cog[num][i] = cog[num][i+1];
+				}
+				cog[num][7] = temp;
 			}
-			cog[num][7] = temp;
-		}
-		break;
+			break;
 		
-		case 1: {
-			int temp = cog[num][7];
-			for (int i = 7 ; i > 0; i--) {
-				cog[num][i] = cog[num][i-1];
-			}
-			cog[num][0] = temp;
-		}	
+			case 1: {
+				int temp = cog[num][7];
+				for (int i = 7 ; i > 0; i--) {
+					cog[num][i] = cog[num][i-1];
+				}
+				cog[num][0] = temp;
+			}	
 		}
-		
 	}
 	
-	
-	
+	static class listto {
+		int num;
+		int dir;
+			public listto(int num, int dir) {
+				this.num = num;
+				this.dir = dir;
+			}
+	}
 }
